e6e90aecf102a90ad782cf943bb079c7a2ecd3e1
Bryan Swagerty <bryan.swagerty@carvana.com>
2019-10-23 23:13:11 -0700

    Hello World!

---
+++ src/GradeBook/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GradeBook
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(args.Length > 0 ? $"Hello, {args[0]}!" : "Hello World!");
+        }
+    }
+}
